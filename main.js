282499fb09aa3bf72348dc4ab48d3c9c565e7682
ishiaryan <ishiaryan@gmail.com>
2019-12-19 16:10:00 -0600

    Initial Project Setup

---
+++ main.js
@@ -0,0 +1,43 @@
+let imageElement;
+let slideNumberElement;
+let backButton;
+let nextButton;
+let slideNumber = 0;
+
+
+window.onload = function() {
+    imageElement = document.getElementById("image");
+    slideNumberElement = document.getElementById("slide-number");
+    backButton = document.getElementById("back");
+    nextButton = document.getElementById("next");
+
+    backButton.onclick = back;
+    nextButton.onclick = next;
+
+}
+
+function next() {
+    if (slideNumber <= 4) { 
+        slideNumber = slideNumber + 1;
+        let filePath = `./resources/pic-${slideNumber}.jpg`;
+        imageElement.src = filePath;
+    
+        slideNumberElement.textContent = `Slide ${slideNumber + 1}/6`;
+    
+    }
+
+}
+
+function back() {
+    if (slideNumber >= 1) {
+        slideNumber = slideNumber - 1;
+        let filePath = `./resources/pic-${slideNumber}.jpg`;
+        imageElement.src = filePath
+
+        slideNumberElement.textContent = `Slide ${slideNumber + 1}/6`;
+
+    }
+    
+
+
+}
